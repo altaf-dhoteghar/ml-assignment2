4f39bcbb311863d58889553cdfe8551449df1735
Altaf Ah Dhoteghar <altafahdhoteghar@Altafs-MacBook-Pro.local>
2026-08-11 01:50:34 +0530

    initial checkin for assignment 2. preparing streamlit website

---
+++ app.py
@@ -0,0 +1,14 @@
+import streamlit as st
+
+
+
+
+# ---------------------------------------------------------
+# Streamlit Web Interface Controls
+# ---------------------------------------------------------
+st.set_page_config(page_title="UCI Mushroom Classification App", layout="wide")
+st.title("UCI Mushroom Dataset - ML Classification Dashboard")
+st.markdown("Implemented as part of Machine Learning Assignment 2.")
+st.sidebar.header("Navigation & Options")
+st.header(f"Model Results:")
+st.markdown("---")
